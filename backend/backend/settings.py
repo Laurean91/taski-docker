a37087d1e23af327ea3de1243521ce1e872666e1
Laurean91 <mkalinin@MacBook-Air-Mihail.local>
2023-07-02 21:58:55 +0300

    rev5

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['laurean2.ddns.net', '158.160.69.137', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 # Application definition
 
